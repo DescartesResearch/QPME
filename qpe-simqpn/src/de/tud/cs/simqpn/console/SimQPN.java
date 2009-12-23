de89687fb13dff0b7656a18fba5cef15645e46d0
skounev <skounev@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2009-12-23 21:00:58 +0000

    Added code in console/SimQPN.java to save simulation results to XML (.simqpn) file.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/qpme/private/skounev/projects/qpme/eclipse-src/trunk@143 9e42b895-fcda-4063-8a3b-11be15eb1bbd

--- qpe-simqpn/src/de/tud/cs/simqpn/console/SimQPN.java
+++ qpe-simqpn/src/de/tud/cs/simqpn/console/SimQPN.java
@@ -1,15 +1,39 @@
+/*
+ * Copyright (c) 2009 Samuel Kounev. All rights reserved.
+ *    
+ * The use, copying, modification or distribution of this software and its documentation for 
+ * any purpose is NOT allowed without the written permission of the author.
+ *  
+ * This source code is provided as is, without any express or implied warranty.
+ *
+ *  History:
+ *  Date        ID                Description
+ *  ----------  ----------------  ------------------------------------------------------------------
+ *  2006/??/??  Christofer Dutz   Created.
+ *  2009/12/23  Samuel Kounev     Added code to save simulation results to XML (.simqpn) file.
+ * 
+ */
+
 package de.tud.cs.simqpn.console;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Iterator;
 
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 import org.dom4j.XPath;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
-
+import org.dom4j.io.XMLWriter;
+ 
 import de.tud.cs.simqpn.kernel.SimQPNException;
 import de.tud.cs.simqpn.kernel.Simulator;
+import de.tud.cs.simqpn.kernel.Stats;
+import de.tud.cs.simqpn.kernel.StatsDocumentBuilder;
 
 public class SimQPN {
 
@@ -18,6 +42,7 @@ public class SimQPN {
 	 */
 	public static void main(String[] args) {
 		Document netDocument = null;
+		Element net;
 		String configuration = null;
 		boolean listConfigurations = false;
 		boolean runSimulation = false;
@@ -73,10 +98,13 @@ public class SimQPN {
 						System.out.println("Running configuration \"" + configuration + "\"");
 						System.out.println();
 						try {
-							Simulator.configure(netDocument.getRootElement(),
-									configuration);
-							Simulator.execute(netDocument.getRootElement(),
-									configuration);
+							net = netDocument.getRootElement();
+							Simulator.configure(net, configuration);							
+							Stats[] result = Simulator.execute(net, configuration);
+							StatsDocumentBuilder builder = new StatsDocumentBuilder(result, net, configuration);
+							Document statsDocument = builder.buildDocument();
+							File resultsFile = new File(Simulator.statsDir, builder.getResultFileBaseName() + ".simqpn");
+							saveXmlToFile(statsDocument, resultsFile);
 						} catch (SimQPNException e) {
 							e.printStackTrace();
 						}
@@ -87,4 +115,22 @@ public class SimQPN {
 			e.printStackTrace();
 		}
 	}
+		
+	private static void saveXmlToFile(Document doc, File file) {
+		XMLWriter writer = null;
+		try {
+			writer = new XMLWriter(new FileWriter(file), OutputFormat.createPrettyPrint());
+			writer.write(doc);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+
 }
