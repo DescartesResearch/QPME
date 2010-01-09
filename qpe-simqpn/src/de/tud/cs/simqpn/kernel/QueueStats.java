86032c30b31f4ba4d8c42b6183499968cdf782d0
skounev <skounev@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2010-01-09 21:45:14 +0000

    Updated source file headers (copyright, license, history).
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/qpme/private/skounev/projects/qpme/eclipse-src/trunk@157 9e42b895-fcda-4063-8a3b-11be15eb1bbd

--- qpe-simqpn/src/de/tud/cs/simqpn/kernel/QueueStats.java
+++ qpe-simqpn/src/de/tud/cs/simqpn/kernel/QueueStats.java
@@ -1,18 +1,44 @@
-/*
- * Copyright (c) 2009 Samuel Kounev. All rights reserved.
- *    
- * The use, copying, modification or distribution of this software and its documentation for 
- * any purpose is NOT allowed without the written permission of the author.
- *  
- * This source code is provided as is, without any express or implied warranty.
+/* ==============================================
+ * QPME : Queueing Petri net Modeling Environment
+ * ==============================================
  *
+ * (c) Copyright 2003-2010, by Samuel Kounev and Contributors.
+ * 
+ * Project Info:   http://descartes.ipd.kit.edu/projects/qpme/
+ *                 http://www.descartes-research.net/
+ *    
+ * All rights reserved. This software is made available under the terms of the 
+ * Eclipse Public License (EPL) v1.0 as published by the Eclipse Foundation
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse Public License (EPL)
+ * for more details.
+ *
+ * You should have received a copy of the Eclipse Public License (EPL)
+ * along with this software; if not visit http://www.eclipse.org or write to
+ * Eclipse Foundation, Inc., 308 SW First Avenue, Suite 110, Portland, 97204 USA
+ * Email: license (at) eclipse.org 
+ *  
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *                                
+ * =============================================
+ *
+ * Original Author(s):  Samuel Kounev
+ * Contributor(s):   
+ * 
+ * NOTE: The above list of contributors lists only the people that have
+ * contributed to this source file - for a list of ALL contributors to 
+ * the project, please see the README.txt file.
+ * 
  *  History:
  *  Date        ID                Description
  *  ----------  ----------------  ------------------------------------------------------------------------------
  *  2008/11/25  Samuel Kounev     Created.
  * 
  */
-
 package de.tud.cs.simqpn.kernel;
 
 /**
