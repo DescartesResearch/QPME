654264572802b1b42a452d0fd24d872e37fc43d8
skounev <skounev@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2010-01-17 20:05:03 +0000

    fixed a minor bug in script
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/qpme/private/skounev/projects/qpme/eclipse-src/trunk@178 9e42b895-fcda-4063-8a3b-11be15eb1bbd

--- qpe-simqpn/SimQPN.sh
+++ qpe-simqpn/SimQPN.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -classpath -server "./lib/colt.jar:./lib/dom4j-1.6.1.jar:./lib/jaxen-1.1-beta-6.jar:./bin/classes/" de.tud.cs.simqpn.console.SimQPN -l -r "example_config" ../examples/ispass03.qpe
+java -server -classpath  "./lib/colt.jar:./lib/dom4j-1.6.1.jar:./lib/jaxen-1.1-beta-6.jar:./bin/classes/" de.tud.cs.simqpn.console.SimQPN -l -r "example_config" ../examples/ispass03.qpe
 
  
 
