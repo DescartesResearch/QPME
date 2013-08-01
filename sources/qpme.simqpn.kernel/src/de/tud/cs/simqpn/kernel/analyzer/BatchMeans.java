37fa06108a503c44f761aac6a85628d872713aa9
Juergen Walter <juergen.walter.halle@gmail.com>
2013-08-01 08:30:23 +0200

    uncommented output + non-future Execution in batch means

--- sources/qpme.simqpn.kernel/src/de/tud/cs/simqpn/kernel/analyzer/BatchMeans.java
+++ sources/qpme.simqpn.kernel/src/de/tud/cs/simqpn/kernel/analyzer/BatchMeans.java
@@ -3,16 +3,14 @@ package de.tud.cs.simqpn.kernel.analyzer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.apache.log4j.Logger;
 
 import de.tud.cs.simqpn.kernel.SimQPNConfiguration;
 import de.tud.cs.simqpn.kernel.SimQPNException;
 import de.tud.cs.simqpn.kernel.entities.Net;
+import de.tud.cs.simqpn.kernel.entities.QPlace;
+import de.tud.cs.simqpn.kernel.entities.queue.Queue;
 import de.tud.cs.simqpn.kernel.executor.parallel.ParallelExecutor;
 import de.tud.cs.simqpn.kernel.executor.sequential.SequentialExecutor;
 import de.tud.cs.simqpn.kernel.monitor.SimulatorProgress;
@@ -32,24 +30,24 @@ public class BatchMeans implements Analyzer {
 		SimulatorResults results = runBatchMeans(net, configuration, monitor);
 
 		List<Stats> stats = new ArrayList<Stats>();
-//		for (int p = 0; p < results.getPlaces().length; p++) {
-//			stats.add(results.getPlaces()[p].placeStats);
-//			if (results.getPlaces()[p] instanceof QPlace) {
-//				stats.add(((QPlace) results.getPlaces()[p]).qPlaceQueueStats);
-//			}
-//			results.getPlaces()[p].report(configuration);
-//		}
-//		if (results.getQueues() != null) {
-//			for (Queue queue : results.getQueues()) {
-//				if (queue != null && queue.queueStats != null) {
-//					stats.add(queue.queueStats);
-//				}
-//			}
-//		}
-//		for (int pr = 0; pr < results.getProbes().length; pr++) {
-//			stats.add(results.getProbes()[pr].probeStats);
-//			results.getProbes()[pr].report(configuration);
-//		}
+		for (int p = 0; p < results.getPlaces().length; p++) {
+			stats.add(results.getPlaces()[p].placeStats);
+			if (results.getPlaces()[p] instanceof QPlace) {
+				stats.add(((QPlace) results.getPlaces()[p]).qPlaceQueueStats);
+			}
+			results.getPlaces()[p].report(configuration);
+		}
+		if (results.getQueues() != null) {
+			for (Queue queue : results.getQueues()) {
+				if (queue != null && queue.getQueueStats() != null) {
+					stats.add(queue.getQueueStats());
+				}
+			}
+		}
+		for (int pr = 0; pr < results.getProbes().length; pr++) {
+			stats.add(results.getProbes()[pr].probeStats);
+			results.getProbes()[pr].report(configuration);
+		}
 		return (Stats[]) stats.toArray(new Stats[stats.size()]);
 	}
 
@@ -67,21 +65,18 @@ public class BatchMeans implements Analyzer {
 		progressMonitor = monitor;
 		progressMonitor.startSimulation(configuration);
 
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		boolean eventLevelParallel = true;
+
 		Callable<Net> run;
 		if(eventLevelParallel){
 			run = new ParallelExecutor(net, configuration, monitor, 1);			
 		}else{
 			run = new SequentialExecutor(net, configuration,monitor,1);			
 		}
-		Future<Net> future = executorService.submit(run);
 		try {
-			net = future.get();
-		} catch (InterruptedException e) {
-			log.error("",e);
-		} catch (ExecutionException e) {
-			log.error("",e);
+			net = run.call();
+		} catch (Exception e) {
+			log.info(e);
 		}
 		
 		progressMonitor.finishSimulation();
