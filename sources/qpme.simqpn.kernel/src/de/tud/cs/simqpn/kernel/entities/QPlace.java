de8273bef7647de5fae52f07e5a037247f8a2be7
Juergen Walter <juergen.walter.halle@gmail.com>
2013-10-01 21:41:34 +0200

    small fix in copy function of QPlace

--- sources/qpme.simqpn.kernel/src/de/tud/cs/simqpn/kernel/entities/QPlace.java
+++ sources/qpme.simqpn.kernel/src/de/tud/cs/simqpn/kernel/entities/QPlace.java
@@ -149,7 +149,7 @@ public class QPlace extends Place {
 			SimQPNConfiguration configuration) throws SimQPNException {
 		QPlace clone = new QPlace(id, name, colors, this.inTrans.length,
 				this.outTrans.length, probeActions[0].length, statsLevel,
-				depDiscip, queues[this.id], element, configuration);
+				depDiscip, queues[queue.id], element, configuration);
 		clone.finishCloning(this, queues, transitions, configuration);
 		return clone;
 	}
