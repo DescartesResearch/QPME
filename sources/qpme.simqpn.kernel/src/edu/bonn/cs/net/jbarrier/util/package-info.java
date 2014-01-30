29dcd0655ca8024aeed9b5e50726317fbc64613a
Simon Spinner <simon.spinner@kit.edu>
2014-01-30 10:44:25 +0100

    Add jbarrier library project.
    Remove dependency on infrastructure

--- sources/qpme.simqpn.kernel/src/edu/bonn/cs/net/jbarrier/util/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Utilities used by some barrier implementations. Currently this package
- * contains only a single class with a few methods for calculating powers-of-two
- * related stuff.
- */
-package edu.bonn.cs.net.jbarrier.util;
