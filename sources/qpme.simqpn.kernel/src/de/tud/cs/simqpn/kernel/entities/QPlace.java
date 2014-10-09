9561c507430ba368d62a8a108719e9efd706a3ad
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2014-10-09 10:50:58 +0200

    Fix lookahead calculation for QPlace.

--- sources/qpme.simqpn.kernel/src/de/tud/cs/simqpn/kernel/entities/QPlace.java
+++ sources/qpme.simqpn.kernel/src/de/tud/cs/simqpn/kernel/entities/QPlace.java
@@ -465,8 +465,7 @@ public class QPlace extends Place {
 		for (int i = 0; i < colors.length; i++) {
 			lookaheads.add(getLookahead(i));
 		}
-		// return Collections.min(lookaheads);
-		return Collections.max(lookaheads);
+		return Collections.min(lookaheads);
 	}
 
 	/**
